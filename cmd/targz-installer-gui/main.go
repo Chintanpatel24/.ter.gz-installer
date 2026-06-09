e21be23b1102803b6a29f291999a527a72b5eac5
Chintan Patel <216989679+Chintanpatel24@users.noreply.github.com>
2026-06-09 18:03:38 +0530

    Update main.go

--- cmd/targz-installer-gui/main.go
+++ cmd/targz-installer-gui/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"context"
+	"errors"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+
+	"github.com/open-source-labs/targz-installer/internal/installer"
+)
+
